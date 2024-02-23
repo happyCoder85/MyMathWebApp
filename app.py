a410f63b12a693b4d3dcf31aff7bb0890fdf35c4
Jonathan Spurling <jon@jonspurling.ca>
2024-02-22 22:20:57 -0600

    Added error handling

--- app.py
+++ app.py
@@ -27,11 +27,29 @@ def do_calculation():
     title = "Here are your results:"
 
     for number in numbers_list:
-        num_list.num_list.append(int(number))
+        try:
+            num_list.num_list.append(int(number))
+        except ValueError:
+            print("Must enter numbers")
+            break
 
-    max_number = max(tuple(num_list.num_list))
-    average = MyMath.calculate_average(num_list)
-    std_deviation = MyMath.calculate_standard_deviation(num_list)
+    try:
+        max_number = max(tuple(num_list.num_list))
+    except ValueError as e: 
+        print("ValueError: ", e)
+        max_number = 0
+    try:
+        average = MyMath.calculate_average(num_list)
+    except ZeroDivisionError as e:
+        print("Error: ", e)
+        num_list.num_list = []
+        average = 0
+    try:
+        std_deviation = MyMath.calculate_standard_deviation(num_list)
+    except UnboundLocalError as e:
+        print("Error: ", e)
+        std_deviation = 0
+
     return render_template('calculate.html',
                         num_list=num_list.num_list,
                         max=max_number,
